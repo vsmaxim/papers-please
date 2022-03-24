e6a3aa0fbdcc2f56e779a6414b01783210a10e36
Maksim Vasilev <maxim.vasilev@jetbrains.com>
2022-03-24 05:30:26 +0100

    Add docker flag

--- papers_please/parser.py
+++ papers_please/parser.py
@@ -44,6 +44,7 @@ class ChromeDriver:
             options.add_argument("--start-maximized")
             options.add_argument("--headless")
             options.add_argument("--disable-gpu")
+            options.add_argument("--disable-dev-shm-usage")
             options.add_argument("--remote-debugging-port=9222")
 
         self._d = webdriver.Chrome(options=options)
