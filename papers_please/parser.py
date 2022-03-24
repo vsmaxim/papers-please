9eb10536b7a9044609bdea773a5288b65b04e8e4
Maksim Vasilev <maxim.vasilev@jetbrains.com>
2022-03-24 05:37:49 +0100

    Chrome no-sandbox

--- papers_please/parser.py
+++ papers_please/parser.py
@@ -45,6 +45,7 @@ class ChromeDriver:
             options.add_argument("--headless")
             options.add_argument("--disable-gpu")
             options.add_argument("--disable-dev-shm-usage")
+            options.add_argument("--no-sandbox")
             options.add_argument("--remote-debugging-port=9222")
 
         self._d = webdriver.Chrome(options=options)
